2e55d2f9fe67a897185c623508a7bc05f86f72a2
Luke <slukehall92@gmail.com>
2026-01-27 10:26:18 -0500

    Updated After Resting

--- script.js
+++ script.js
@@ -1,18 +1,90 @@
 const myLibrary = [];
+const div=document.querySelector('.bookList');
+const button = document.getElementById('addButton');
+const input = document.querySelector('input');
 
-
-function Book(title, author, pages, genre){
+function Book(title, author, pages, character){
     this.title=title;
     this.author=author;
     this.pages=pages;
-    this.genre=genre;
+    this.character=character;
 }
 
-function addBookToLibrary(){
 
-}
 
 //the key is to make a function that creates the book, then adds it to the Library
 
-let bible= new Book('The Holy Bible: KJV Version', 'Jesus Christ', 2000, 'religion');
-console.log(bible);
+let animorph1= new Book('Animorphs : The Invasion', 'K.A. Applegate', 184, 'Jake');
+let animorph2 = new Book('Animorphs: The Visitor', 'K.A. Applegate', 175, 'Rachel')
+let animorph3 = new Book('Animorphs: The Encounter', 'K.A. Applegate', 150, 'Tobias');
+let animorph4 = new Book('Animorphs: The Message', 'K.A. Applegate', 151, 'Cassie');
+let animorph5 = new Book('Animorphs: The Predator', 'K.A. Applegate', 152, 'Marco');
+
+
+
+//PsuedoCode: 
+//Need a card to be created in bookCard
+//Each card is going to be a little box containing: title, author, pages, character
+
+const titleInfo = document.getElementById('title');
+const authorInfo = document.getElementById('author');
+const pagesInfo = document.getElementById('pages');
+const characterInfo = document.getElementById('character');
+
+
+
+
+
+//LOGIC FOR ADDING BOOKS//
+
+button.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    const card = document.createElement('div');
+    const span = document.createElement('p');
+    const span2= document.createElement('p');
+    const span3 = document.createElement('p');
+    const span4 = document.createElement('p');
+
+    span.style.width='40%';
+    span2.style.width='20%';
+    span3.style.width='20%';
+    span4.style.width= '20%';
+    
+    const title = titleInfo.value;
+    const author = authorInfo.value;
+    const pages = pagesInfo.value;
+    const character= characterInfo.value;
+
+    title.value= '';
+    author.value= '';
+    pages.value='';
+    character.value='';
+    
+    
+    if(card){
+        card.style.width='100%';
+        card.style.display='flex';
+        card.style.border='2px solid white';
+        card.style.borderLeft='none';
+        card.style.marginBottom='5px';
+        card.style.backgroundColor='white';
+
+        card.appendChild(span);
+        span.textContent=title;
+        card.appendChild(span2);
+        span2.textContent=author;
+        card.appendChild(span3);
+        span3.textContent=pages;
+        card.appendChild(span4);
+        span4.textContent=character;
+
+
+    }
+
+    
+    div.appendChild(card);
+
+    
+
+});
